e72b1f6b92b923a40695285cdcad50bfe05abae8
aprilholman <holman.april@awatapu.school.nz>
2022-04-01 09:33:56 +1300

    First version, very basic

---
+++ user_age_1.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you?"))
+if age <= 11:
+    print("You are too young to be doing this")
+elif age >= 131:
+    print("Please enter a valid age between(12 and 130)")
+else:
+    print(age)
