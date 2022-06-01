cbb84aa90046983a6189512b86a2df7344af98d8
aprilholman <holman.april@awatapu.school.nz>
2022-06-02 09:31:03 +1200

    .

--- movie_fundraiser.py
+++ movie_fundraiser.py
@@ -1,5 +1,6 @@
 # Functions
 def not_blank(question, error):
+    global user_list
     valid = False
     # Asks user what their name is
     print(question)
@@ -20,51 +21,54 @@ def not_blank(question, error):
             if spaces >= 1:
                 # If spaces is more than 0, it reprints name and ends the function
                 print(user_name)
+                user_list.append(user_name)
                 return user_name
             else:
                 # If spaces = equal 0, it prints error and then asks for input again
                 print(error)
 
 
-def user_age(question, error, error_2):
-    global age
+def ticket_price_amount(question, error, error_2):
+    global cost
+    global t_amount
+    cost = 0
     valid = False
-    print(question)
-    # Continues to ask for input until valid input it entered
+    t_amount = 0
     while not valid:
         try:
-            # Asks for age
-            age = int(input())
-            if age <= 11:
-                # If too young, code will finish
-                print(error_2)
-                return age
-            elif age >= 131:
-                # If too old, user will be re-asked
-                print(error)
-            else:
-                # If valid age, code finishes
-                return age
+            t_amount = int(input(question))
+            for i in range(t_amount):
+                age = int(input("What age will ticket {} be for?".format(i + 1)))
+                try:
+                    # Asks for age
+                    if age <= 11:
+                        # If too young, code will finish
+                        print(error)
+                        return age
+                    elif age >= 131:
+                        # If too old, user will be re-asked
+                        print(error)
+                        return age
+                    else:
+                        if age < 16:
+                            print("Ticket {} will cost $7.50\n".format(i + 1))
+                            cost += 7.5
+                        elif 16 <= age <= 64:
+                            print("Ticket {} will cost $10.50\n".format(i + 1))
+                            cost += 10.5
+                        else:
+                            print("Ticket {} will cost $6.50\n".format(i + 1))
+                            cost += 6.5
+                except ValueError:
+                    # Prints error message if string is input
+                    print(error_2)
+                valid = True
         except ValueError:
-            # Prints error message if string is input
-            print(error)
-
-
-def ticket_price():
-    global cost
-    global age
-    cost = 0
-    # If age is less than 16 cost = 7.5
-    if age < 16:
-        cost += 7.5
-    # If age is between 16 and 64 cost = 10.5
-    elif 16 <= age <= 64:
-        cost += 10.5
-    # Anything else = 6.5
-    else:
-        cost += 6.5
-    print("Your ticket will cost ${:.2f}\n".format(cost))
-    return cost
+            print(error_2)
+        if valid:
+            user_list.append(t_amount)
+            user_list.append(cost)
+            print("Total cost of {} tickets is ${:.2f}".format(t_amount, cost))
 
 
 def snacks(question_1, question_2, question_3, error):
@@ -84,6 +88,8 @@ def snacks(question_1, question_2, question_3, error):
                     if user_choice >= 6 or user_choice < 0 or user_snack_amount > 5 or user_snack_amount < 1:
                         print(error)
                     else:
+                        user_list.append(user_snack_amount)
+                        user_list.append(snack_choices[user_choice - 1])
                         snack_price = snack_prices[user_choice - 1] * user_snack_amount
                         snack_price_total += snack_price
                         print("Your choice of {} {} costs: ${:.2f}\n".format(user_snack_amount,
@@ -116,32 +122,46 @@ def payment(question, error):
 
 
 def profit():
+    global t_amount
     snack_price_total_profit = snack_price_total * 0.2
-    ticket_profit = cost - 5
+    ticket_profit = cost - (5 * t_amount)
     total_price = snack_price_total + cost
     if payment_method == "Credit":
         surcharge = total_price * 0.05
         total_price += surcharge
     print("The total price is: ${}".format(total_price))
     total_profit = snack_price_total_profit + ticket_profit
+    # user_list.append(total_profit)
+    # user_list.append(total_price)
     print("Profit: ${}".format(total_profit))
 
 
 # Main Routine
+
+# load initial data
+user_list_together = [['April Holman', 3, 'Pitachips']]
+
+# for each ticket order
+user_list = []
+global t_amount
 global snack_price_total
 global cost
 global payment_method
 not_blank("What is your name?", "Please enter a valid full name(first and last name)")
-age = user_age("How old are you?", "Please enter a valid age between(12 and 130)",
-               "You are too young to be doing this")
-if age >= 12:
-    ticket_price()
-    snacks("Do you want to order some/more snacks?\n", "Pick a snack(pick the number you want)\n\nThe options are:\n"
-           "1. Popcorn: $2.50\n2. M&M: $3.00\n3. Pitachips: $4.50\n4. Orange Juice: $3.25\n5. Water: $2.00\n",
-           "Choose an amount(maximum is 5)", "Please enter a valid snack number")
-    payment("Will you be paying cash or credit?(enter 1 or 2)\nIf paying with credit "
-            "there will be a surcharge of 2% to the final price\nOption 1                "
-            "Option 2\nCash                    Credit", "Please enter a valid input(1 or 2)"
-            "\nOption 1                "
-            "Option 2\nCash                    Credit")
-    profit()
+ticket_price_amount("How many tickets do you want", "That is not a valid age(Please rerun the programme)",
+                    "Please enter valid input")
+snacks("Do you want to order some/more snacks?\n", "Pick a snack(pick the number you want)\n\nThe options are:\n"
+       "1. Popcorn: $2.50\n2. M&M: $3.00\n3. Pitachips: $4.50\n4. Orange Juice: $3.25\n5. Water: $2.00\n",
+       "Choose an amount(maximum is 5)", "Please enter a valid snack number")
+payment("Will you be paying cash or credit?(enter 1 or 2)\nIf paying with credit "
+        "there will be a surcharge of 5% to the final price\nOption 1                "
+        "Option 2\nCash                    Credit", "Please enter a valid input(1 or 2)"
+                                                    "\nOption 1                "
+                                                    "Option 2\nCash                    Credit")
+# calculate total profit
+user_list_together.append(user_list)
+print(user_list)
+print(user_list_together)
+
+profit()
+
